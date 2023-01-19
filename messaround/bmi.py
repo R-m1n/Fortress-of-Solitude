3b6bc93af81a7ab9234469122db3ecc4c1094947
Armin Abbasi <apmnh.armin@gmail.com>
2023-01-19 13:59:17 +0100

    Add bmi.py

---
+++ messaround/bmi.py
@@ -0,0 +1,71 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+app = customtkinter.CTk()
+app.geometry("360x200")
+app.title("BMI Calculator")
+
+
+def button_callback():
+    weight = 0 if entry_1.get() == '' else float(entry_1.get())
+    height = 1 if entry_2.get() == '' else float(entry_2.get())
+
+    wmetric = combobox_1.get()
+    if wmetric == "g":
+        weight /= 1000
+
+    if wmetric == "lbs":
+        weight /= 2.205
+
+    hmetric = combobox_2.get()
+    if hmetric == "cm":
+        height /= 100
+
+    if hmetric == "inch":
+        height /= 39.37
+
+    bmi = weight / pow(height, 2)
+
+    create_toplevel(bmi)
+
+    entry_1.delete(0, len(entry_1.get()))
+    entry_2.delete(0, len(entry_2.get()))
+
+
+def create_toplevel(bmi):
+    window = customtkinter.CTkToplevel()
+    window.title('')
+
+    label = customtkinter.CTkLabel(window, text=f"BMI:\t{bmi}")
+    label.pack(side="top", fill="both", expand=True, padx=40, pady=40)
+
+
+frame_1 = customtkinter.CTkFrame(master=app)
+frame_1.grid(row=0, column=0, pady=20, padx=60)
+
+frame_2 = customtkinter.CTkFrame(master=app, fg_color="transparent")
+frame_2.grid(row=1, column=0, padx=60)
+
+entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="Weight")
+entry_1.grid(row=0, column=0, pady=10, padx=10)
+
+combobox_1 = customtkinter.CTkComboBox(
+    frame_1, width=60, values=["kg", "g", "lbs"])
+combobox_1.grid(row=0, column=1, pady=10, padx=10)
+combobox_1.set("kg")
+
+entry_2 = customtkinter.CTkEntry(master=frame_1, placeholder_text="Height")
+entry_2.grid(row=1, column=0, pady=10, padx=10)
+
+combobox_2 = customtkinter.CTkComboBox(
+    frame_1, width=60, values=["m", "cm", "inch"])
+combobox_2.grid(row=1, column=1, pady=10, padx=10)
+combobox_2.set("m")
+
+button_1 = customtkinter.CTkButton(
+    master=frame_2, command=button_callback, text="Calculate")
+button_1.grid(row=0, column=0, pady=10, padx=10)
+
+app.mainloop()
