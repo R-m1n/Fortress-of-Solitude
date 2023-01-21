a78431a1d377a82db0c998646e8147a228c32d50
Armin Abbasi <apmnh.armin@gmail.com>
2023-01-21 21:02:35 +0100

    Add feature to vectools.py

--- messaround/vectools.py
+++ messaround/vectools.py
@@ -1,10 +1,57 @@
 
+from math import sqrt
+from pprint import pprint
+
+from numpy import arccos
+
+
+class Unit():
+    def __init__(self, i: int | float, j: int | float, k: int | float) -> None:
+        self.i = i
+        self.j = j
+        self.k = k
+
+    def __str__(self) -> str:
+        return f"Unit({self.i}, {self.j}, {self.k})"
+
+    def __repr__(self) -> str:
+        if self.j > 0 and self.k > 0:
+            return f"{self.i}i - {self.j}j + {self.k}k"
+
+        elif self.j < 0 and self.k > 0:
+            return f"{self.i}i + {abs(self.j)}j + {self.k}k"
+
+        elif self.j > 0 and self.k < 0:
+            return f"{self.i}i - {self.j}j - {abs(self.k)}k"
+
+        elif self.j < 0 and self.k < 0:
+            return f"{self.i}i + {abs(self.j)}j - {abs(self.k)}k"
+
 
 class Vector3():
     def __init__(self, x: int | float, y: int | float, z: int | float) -> None:
         self.x = x
         self.y = y
         self.z = z
+
+    def unit(self):
+        return self * (1 / abs(self))
+
+    def cross(self, other) -> Unit:
+        i = (self.y * other.z) - (self.z * other.y)
+        j = (self.x * other.z) - (self.z * other.x)
+        k = (self.x * other.y) - (self.y * other.x)
+
+        return Unit(i, j, k)
+
+    def dist(self, other) -> float:
+        return abs(Vector3(self.x - other.x, self.y - other.y, self.z - other.z))
+
+    def angle(self, other) -> float:
+        return arccos((self * other) / (abs(self) * abs(other)))
+
+    def proj(self, other):
+        return other * ((self * other) / (other * other))
 
     def __str__(self) -> str:
         return f"Vector3({self.x}, {self.y}, {self.z})"
@@ -25,8 +72,20 @@ class Vector3():
         elif isinstance(other, int) or isinstance(other, float):
             return Vector3(other * self.x, other * self.y, other * self.z)
 
+    def __eq__(self, other):
+        return abs(self) == abs(other)
+
+    def __ge__(self, other):
+        return abs(self) >= abs(other)
+
+    def __gt__(self, other):
+        return abs(self) > abs(other)
+
+    def __abs__(self):
+        return sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
+
 
 v1 = Vector3(2, 4, 5)
 v2 = Vector3(7, 2, 6)
 
-print(v1 + v2)
+pprint(v1.proj(v2))
